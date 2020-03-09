ea306b0331f9169f784b64170afb057358e1e53b
Huang Jian-Min <yfr.huang@hotmail.com>
2020-03-10 01:21:25 +0800

    feat: Init Project
    * init project with spring boot maven
    * add jib

---
+++ src/main/java/tw/b2e/Event20201JavaApplication.java
@@ -0,0 +1,13 @@
+package tw.b2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Event20201JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Event20201JavaApplication.class, args);
+    }
+
+}
