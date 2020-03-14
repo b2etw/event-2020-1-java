3c6a7a7991e93fbe1b91d1ba673f9aa7ddd87d14
Huang Jian-Min <yfr.huang@hotmail.com>
2020-03-14 18:30:07 +0800

    feat: Add init setting

--- src/main/java/tw/b2e/Event20201JavaApplication.java
+++ src/main/java/tw/b2e/Event20201JavaApplication.java
@@ -2,8 +2,10 @@ package tw.b2e;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Event20201JavaApplication {
 
     public static void main(String[] args) {
