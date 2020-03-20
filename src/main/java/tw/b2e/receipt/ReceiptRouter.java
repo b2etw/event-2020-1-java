b9ee08376357cabca7f27f8ed9401b0add6dfbd1
yehche01 <42378938+yehche01@users.noreply.github.com>
2020-03-21 01:28:55 +0800

    WIP (#10)
    * WIP
    
    * fix: Update maven.yml
    
    * Update src/main/java/tw/b2e/receipt/PrizeNumberObject.java
    
    Co-Authored-By: 黃健旻 <yfr.huang@hotmail.com>
    
    * Update pom.xml
    
    Co-Authored-By: 黃健旻 <yfr.huang@hotmail.com>
    
    * Update pom.xml
    
    Co-Authored-By: 黃健旻 <yfr.huang@hotmail.com>
    
    * Update src/main/java/tw/b2e/receipt/PrizeNumberObject.java
    
    Co-Authored-By: 黃健旻 <yfr.huang@hotmail.com>
    
    * Update src/main/java/tw/b2e/receipt/ReceiptRouter.java
    
    Co-Authored-By: 黃健旻 <yfr.huang@hotmail.com>
    
    Co-authored-by: 黃健旻 <yfr.huang@hotmail.com>

--- src/main/java/tw/b2e/receipt/ReceiptRouter.java
+++ src/main/java/tw/b2e/receipt/ReceiptRouter.java
@@ -6,4 +6,11 @@ import tw.b2e.common.Router;
 
 @Component
 public class ReceiptRouter implements Router<SlashCommandRequest> {
+@Override
+    public String handle(SlashCommandRequest sReq){
+        String aa = sReq.getRequestBodyAsString();
+        System.out.println(aa);
+        return "Coming soon";
+    }
+
 }
