8479e4b960fd3103f34477faf5e42be2f55b0aad
yihong.gao <luciferstut@gmail.com>
2020-04-07 19:09:29 +0800

    添加註解

--- src/main/java/tw/b2e/receipt/common/CommandParamParser.java
+++ src/main/java/tw/b2e/receipt/common/CommandParamParser.java
@@ -3,10 +3,22 @@ package tw.b2e.receipt.common;
 import org.springframework.stereotype.Component;
 
 import tw.b2e.receipt.vo.CommandParam;
-
+/**
+ * 
+ * @author NaiHua
+ * 目的:解析slash command指令參數
+ * 範例:
+ * 	/receipt help => CommandParam物件 {"action":"help"}
+ *  /receipt win -y 2019 -m 02 => CommandParam物件 {"action":"win", "params":{"-y":"2020", "-m":"02"}}
+ */
 @Component
 public class CommandParamParser {
 		
+	/**
+	 * 解析slash command參數
+	 * @param paramString
+	 * @return CommandParam
+	 */
 	public CommandParam Parse(String paramString) {
 		CommandParam result = new CommandParam();
 		
