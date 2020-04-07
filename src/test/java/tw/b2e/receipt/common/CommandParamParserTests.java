1c9f64b42187be3adda3cf489f6760717c4ccdf5
yihong.gao <luciferstut@gmail.com>
2020-04-07 10:44:19 +0800

    新增command參數解析器

---
+++ src/test/java/tw/b2e/receipt/common/CommandParamParserTests.java
@@ -0,0 +1,62 @@
+package tw.b2e.receipt.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import tw.b2e.receipt.vo.CommandParam;
+
+@SpringBootTest
+class CommandParamParserTests {
+
+	@Autowired
+	CommandParamParser commandParamParser;
+	
+	@Test
+	void Should_GetParam_When_OnlyAction() {
+		String paramString = "help";
+		CommandParam param = commandParamParser.Parse(paramString);
+		assertEquals(paramString, param.getAction());
+	}
+
+	@Test
+	void Should_GetParam_When_ActionWithSingleParam() {
+		String action = "win";
+		String[] paramKeys = new String[] {"-y"};
+		String[] paramValues = new String[] {"2020"};
+		
+		StringBuffer paramString = new StringBuffer(action);
+		
+		for(int i = 0; i < paramKeys.length; i++) {
+			paramString.append(String.format(" %s %s", paramKeys[i], paramValues[i]));
+		}
+		
+		CommandParam param = commandParamParser.Parse(paramString.toString());
+		assertEquals(action, param.getAction());
+		
+		for(int i = 0; i < paramKeys.length; i++) {
+			assertEquals(paramValues[i], param.getParam(paramKeys[i]));
+		}
+	}
+	
+	@Test
+	void Should_GetParam_When_ActionWithMultipleParam() {
+		String action = "win";
+		String[] paramKeys = new String[] {"-y", "-m", "-i"};
+		String[] paramValues = new String[] {"2020", "04", "AB12345678"};
+		
+		StringBuffer paramString = new StringBuffer(action);
+		
+		for(int i = 0; i < paramKeys.length; i++) {
+			paramString.append(String.format(" %s %s", paramKeys[i], paramValues[i]));
+		}
+		
+		CommandParam param = commandParamParser.Parse(paramString.toString());
+		assertEquals(action, param.getAction());
+		
+		for(int i = 0; i < paramKeys.length; i++) {
+			assertEquals(paramValues[i], param.getParam(paramKeys[i]));
+		}
+	}
+}
