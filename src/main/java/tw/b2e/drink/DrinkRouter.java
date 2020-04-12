ea3f11a72999371145f61e2578559bb9957f2227
JimLee <chunyin.lee@thinkpower-info.com>
2020-04-12 22:42:32 +0800

    init functionMap

--- src/main/java/tw/b2e/drink/DrinkRouter.java
+++ src/main/java/tw/b2e/drink/DrinkRouter.java
@@ -4,6 +4,27 @@ import com.slack.api.bolt.request.builtin.SlashCommandRequest;
 import org.springframework.stereotype.Component;
 import tw.b2e.common.Router;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class DrinkRouter implements Router<SlashCommandRequest> {
+
+    private static Map<String, String> drinkFunctionMap;
+
+    private static Map<String,String> initDrinkFunctionMap(){
+        if (drinkFunctionMap == null) {
+            drinkFunctionMap = new HashMap<>();
+            drinkFunctionMap.put("-help", "");
+            drinkFunctionMap.put("-find", "");
+            //TODO
+        }
+        return drinkFunctionMap;
+    }
+
+    @Override
+    public String handle(SlashCommandRequest req) {
+        //TODO
+        return null;
+    }
 }
